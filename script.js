ae3507e79cbb61e642c189668e01db5871348180
timo9939 <timothylau20199@gmail.com>
2024-07-20 19:54:41 +0100

    Display question and choices

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const button=document.getElementsByClassName('btn-style');
-console.log("js worked")
 
-// Check whther the answer is correct
-const ButtonArray=Array.from(button)
-ButtonArray.forEach(btn => {
+
+const questions=[
+    {
+        question:"Which one is not the HACCP 7 principles?",
+        choices:[
+            {text:"Conduct Hazard Analysis",correct:false},
+            {text:"Establish Control Points",correct:false},
+            {text:"Establish Corrective Action",correct:false},
+            {text:"Establish Profit Plan",correct:true}
+        ]
+    },
+    {
+        question:"Which measures below is to establish a critical control points?",
+        choices:[
+            {text:"Cooking a chicken to a specfic temperature",correct:true},
+            {text:"Storing Sashimi at room temperature",correct:false},
+            {text:"Checking the cost of the thermal processing",correct:false},
+            {text:"Making the package looks prettier",correct:false}
+        ]
+    }
+]
+
+let currentQuestionIndex=0;
+let score=0;
+
+const questionDisplay=document.getElementById('question')
+const answerChoices=document.getElementById("choices")
+
+// Showing the question and answer choices
+questionDisplay.innerHTML=questions[0].question
+
+questions[0].choices.forEach(element => {
+    const answerButton=document.createElement('button')
+   answerButton.textContent=element.text
+   answerChoices.appendChild(answerButton)
    
-    btn.addEventListener("click",function(){
-
-        if(this.getAttribute('data-correct')==='true'){
-this.classList.add('correct')
-// btn.classList.add('no-hover-correct')
-        }
-        else{
-            this.classList.add('wrong')
-            btn.classList.add('no-hover-correct')
-        }
-
-       Array.from(button).forEach(btn=>{
-        btn.disabled=true
-        btn.classList.add('no-hover')
-        
-       })
-    })
 });
+
+
